bcbdae258cef99e9b3dd55a02eb6e65b8e08f07c
Manas Das <noragami0021@gmail.com>
2024-07-26 19:41:15 +0530

    add database connection

--- server/api/index.js
+++ server/api/index.js
@@ -1,14 +1,24 @@
-//importting packages
+//importing packages
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+//importing utils
+import { connectDB } from "../config/dbConnection.js";
 
 //creating instance
 dotenv.config();
+connectDB();
 const app = express();
 
 //starting server
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 5001;
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running at ${PORT}`);
-})
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/users");
+
+app.listen(port, () => {
+  console.log(`Server is running at ${port}`);
+});
