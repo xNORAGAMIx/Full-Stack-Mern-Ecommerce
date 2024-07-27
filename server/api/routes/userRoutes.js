b9a1a5caa91c3a77193c00bf96f81708ebe5a12a
Manas Das <noragami0021@gmail.com>
2024-07-27 10:03:02 +0530

    add user sign in route

--- server/api/routes/userRoutes.js
+++ server/api/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { createUser } from "../controller/userController.js";
+import { createUser, loginUser } from "../controller/userController.js";
 
 const router = express.Router();
 
 router.route("/").post(createUser);
+
+router.route("/auth").post(loginUser);
 
 export default router;
