8cff463930b47943c04ac48a850e19c38795d710
Manas Das <noragami0021@gmail.com>
2024-07-27 09:41:36 +0530

    set user route for creating user

---
+++ server/api/routes/userRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { createUser } from "../controller/userController.js";
+
+const router = express.Router();
+
+router.route("/").post(createUser);
+
+export default router;
