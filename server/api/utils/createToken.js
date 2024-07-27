5312692bd47ee0e744061bd9e59b180314d0fc8b
Manas Das <noragami0021@gmail.com>
2024-07-27 09:41:17 +0530

    create jwt token

---
+++ server/api/utils/createToken.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_TOKEN, {
+    expiresIn: "30d",
+  });
+
+  //set jwt as htp only cookie
+  res.cookie("jwt", token, {
+    sameSite: "strict",
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    path: "/",
+  });
+
+  return token;
+};
+
+export default generateToken;
