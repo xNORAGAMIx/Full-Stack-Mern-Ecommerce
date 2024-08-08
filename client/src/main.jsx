7705d8ff53f57b11094b035ab4375cc25da6ab14
Manas Das <noragami0021@gmail.com>
2024-08-08 11:10:54 +0530

    default code for get user

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -21,14 +21,23 @@ import PrivateRoute from "./components/PrivateRoute.jsx";
 //User
 import Profile from "./pages/User/Profile.jsx";
 
+//Admin
+import AdminRoutes from "./pages/Admin/AdminRoutes.jsx";
+import UserList from "./pages/Admin/UserList.jsx";
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
       <Route path="" element={<PrivateRoute />}>
         <Route path="/profile" element={<Profile />}/>
       </Route>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+
+      <Route path="/admin" element={<AdminRoutes />}>
+        <Route path="userlist" element={<UserList />}/>
+      </Route>
     </Route>
   )
 );
