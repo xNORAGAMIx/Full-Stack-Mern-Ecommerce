58e7a870cd2938f4c27127851292a18e626d3ba3
Manas Das <noragami0021@gmail.com>
2024-08-11 09:34:03 +0530

    image upload using multer

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 //importing utils
 import { connectDB } from "../config/dbConnection.js";
@@ -10,6 +11,7 @@ import { connectDB } from "../config/dbConnection.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 //creating instance
 dotenv.config();
@@ -26,6 +28,10 @@ app.use(cookieParser());
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/upload", uploadRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
