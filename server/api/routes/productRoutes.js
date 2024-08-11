f1b8800bef60a1c46feb9ca4511ebb177aad3366
Manas Das <noragami0021@gmail.com>
2024-08-11 08:11:11 +0530

    update product

--- server/api/routes/productRoutes.js
+++ server/api/routes/productRoutes.js
@@ -1,10 +1,17 @@
 import express from "express";
 import formidable from "express-formidable";
-import { addProduct } from "../controller/productController.js";
+import {
+  addProduct,
+  updateProductDetails,
+} from "../controller/productController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").post(authenticate, authorizeAdmin, formidable(), addProduct);
+
+router
+  .route("/:id")
+  .put(authenticate, authorizeAdmin, formidable(), updateProductDetails);
 
 export default router;
