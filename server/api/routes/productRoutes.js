090719f29ebdfcb334431cdaac4808805e563861
Manas Das <noragami0021@gmail.com>
2024-08-11 09:04:25 +0530

    routes for top, new products, filter products, get certain products, filtered products, add review

--- server/api/routes/productRoutes.js
+++ server/api/routes/productRoutes.js
@@ -6,19 +6,34 @@ import {
   fetchProductById,
   fetchAllProducts,
   removeProduct,
+  addProductReview,
+  fetchNewProducts,
+  fetchProducts,
+  fetchTopProducts,
+  filterProducts,
 } from "../controller/productController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+import checkId from "../middlewares/checkId.js";
 
 const router = express.Router();
 
-router.route("/").post(authenticate, authorizeAdmin, formidable(), addProduct);
+router
+  .route("/")
+  .get(fetchProducts)
+  .post(authenticate, authorizeAdmin, formidable(), addProduct);
 
 router.route("/allproducts").get(fetchAllProducts);
+router.route("/:id/reviews").post(authenticate, checkId, addProductReview);
+
+router.get("/top", fetchTopProducts);
+router.get("/new", fetchNewProducts);
 
 router
   .route("/:id")
   .get(fetchProductById)
   .put(authenticate, authorizeAdmin, formidable(), updateProductDetails)
   .delete(authenticate, authorizeAdmin, removeProduct);
+
+router.route("/filtered-products").post(filterProducts);
 
 export default router;
