599be0f27d81856f19e837852565ee956eec9321
Manas Das <noragami0021@gmail.com>
2024-08-14 08:55:28 +0530

    check later

---
+++ client/src/redux/api/productApiSlice.js
@@ -0,0 +1,22 @@
+import { PRODUCT_URL, UPLOAD_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const productApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: ({ keyword }) => ({
+        url: `${PRODUCT_URL}`,
+        params: { keyword },
+      }),
+      keepUnusedDataFor: 5,
+      providesTags: ["Products"],
+    }),
+
+    getProductById: builder.query({
+      query: (productId) => `${PRODUCT_URL}/${productId}`,
+      providesTags: (result, error, productId) => [
+        { type: "Product", id: productId },
+      ],
+    }),
+  }),
+});
